ff513263afb2f2e4f143a97fdc96035676d18745
Luc Saffre <luc.saffre@gmail.com>
2021-02-15 12:08:58 +0200

    remove deprecation warnings

--- lino_weleup/lib/pcsw/models.py
+++ lino_weleup/lib/pcsw/models.py
@@ -2,7 +2,7 @@
 # Copyright 2014-2020 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from lino.api import dd
 
