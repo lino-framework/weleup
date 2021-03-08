4443c4d433c515d9d7c6f1688830be89e751d707
Luc Saffre <luc.saffre@gmail.com>
2021-03-08 19:03:55 +0200

    Fix Unknown language code 'nl' (must be one of ['en', 'de', 'fr'])

--- lino_weleup/settings.py
+++ lino_weleup/settings.py
@@ -4,16 +4,11 @@
 from lino_welfare.modlib.welfare.settings import *
 
 class Site(Site):
-    languages = 'de fr en'
-    hidden_languages = None
     # use_websockets = True
 
     help_url = "http://de.welfare.lino-framework.org"
     demo_fixtures = """std std2 few_languages props all_countries
     demo payments demo2 cbss checkdata checksummaries""".split()
-
-    def get_default_language(self):
-        return 'de'
 
     def get_apps_modifiers(self, **kw):
         kw = super(Site, self).get_apps_modifiers(**kw)
